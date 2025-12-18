18fd1be08dec3d82e85ea7a5d84170cc34d8503f
felipeacx <juanfe1190@gmail.com>
2025-12-17 22:11:30 -0500

    feat: crear página de inicio de sesión con carga dinámica del contenido

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import dynamic from "next/dynamic"
+
+const LoginContent = dynamic(() => import("./LoginContent").then((mod) => mod.LoginContent), {
+  ssr: false,
+  loading: () => <div className="min-h-screen" />,
+})
+
+export default function LoginPage() {
+  return <LoginContent />
+}
